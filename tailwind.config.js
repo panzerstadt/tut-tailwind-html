d46058f0c2578d7baaf2db9d87be852d8a22b8be
panzerstadt <tliqun@gmail.com>
2020-01-16 21:01:54 +0900

    responsive, animation, transitions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   theme: {
+    animations: {
+      pulse: {
+        from: {
+          opacity: 0.3
+        },
+        to: {
+          opacity: 1
+        }
+      }
+    },
     extend: {}
   },
-  variants: {},
-  plugins: []
-}
+  variants: {
+    backgroundColor: ["responsive", "hover", "focus", "active"]
+  },
+  plugins: [
+    require("tailwindcss-transitions")(),
+    require("tailwindcss-animations")()
+  ]
+};
